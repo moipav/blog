0ccd23aa5a4c2eaee4a63afc8c5a500379ce4162
moipav <moipav@ya.ru>
2024-03-13 10:17:56 +0800

    add simple routing

--- public/index.php
+++ public/index.php
@@ -1,17 +1,47 @@
 <?php
-
-use P\Blog\Controllers\HomeController;
-use function Tamtamchik\SimpleFlash\flash;
-
 if (!session_id()) @session_start();
 
 require_once '../vendor/autoload.php';
 
-$home = new HomeController();
-echo  $home->index();
+use P\Blog\Controllers\HomeController;
+use function Tamtamchik\SimpleFlash\flash;
 
-//Kint::dump($home);
-try {
-    echo flash()->error('hello');
-} catch (\Tamtamchik\SimpleFlash\Exceptions\FlashTemplateNotFoundException $e) {
+
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', ['P\Blog\Controllers\HomeController', 'index']);
+    // {id} must be a number (\d+)
+    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+//    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        echo '404 Not Found';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        $method = $handler[1];
+        d($handler[0]);
+        $class = new $handler[0];
+        call_user_func([$class, $method], $vars);
+        // ... call $handler with $vars
+        break;
 }
